5460a72db7623221b5d3ec531fb433e7bcfb3472
Kaique <kaique@neurony.com.br>
2022-05-19 21:05:52 -0300

    Adiciona página de not found

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
 import Header from './components/Header';
-
+import Erro from './pages/Erro';
 
 function RoutesApp() {
     return(
@@ -12,6 +12,8 @@ function RoutesApp() {
             <Routes>
                 <Route path="/" element={<Home/>}></Route>
                 <Route path="/filme/:id" element={<Filme/>}></Route>
+
+                <Route path="*" element={<Erro/>}></Route>
             </Routes>
         </BrowserRouter>
     )
