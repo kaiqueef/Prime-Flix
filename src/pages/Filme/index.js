969be59365b2ef919e15b5165635e2a3ca0ac978
Kaique <kaique@neurony.com.br>
2022-05-20 21:33:37 -0300

    Utiliza navigate para pagina de filme não encontrada e adiciona link de trailer

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 import './filme-info.css';
 import api from "../../services/api";
 
 function Filme() {
     const { id } = useParams();
+    const navigate = useNavigate();
+
     const [movie, setMovie] = useState({});
     const [loading, setLoading] = useState(true);
 
@@ -21,8 +23,9 @@ function Filme() {
                     setLoading(false);
                 })
                 .catch(() => {
-                    setLoading(false);
-                    console.log("Filme não encontrado")
+                    console.log("Filme não encontrado");
+                    navigate("/", { replace: false });
+                    return
                 })
         }
 
@@ -31,7 +34,7 @@ function Filme() {
         return () => {
             console.log("Componente foi desmontado")
         }
-    }, [])
+    }, [navigate, id])
 
 
     if (loading) {
@@ -52,7 +55,7 @@ function Filme() {
             <div className="area-buttons">
                 <button>Salvar</button>
                 <button>
-                    <a href="#">
+                    <a target="_blank" rel="external" href={`https://youtube.com/results?search_query=${movie.title}+Trailer`}>
                         Trailer
                     </a>
                 </button>
