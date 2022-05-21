3a586cfa9a0ea410ead1e84108f47e9fc1aab290
Kaique <kaique@neurony.com.br>
2022-05-21 17:20:12 -0300

    Deixa a mais similar com a netflix

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,27 +3,45 @@ import api from '../../services/api'
 import { Link } from "react-router-dom";
 import './home.css'
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+
 // https://api.themoviedb.org/3/movie/popular?api_key=7a8e988df0c98a78f5908610d79cf071&language=pt-BR
 
 function Home() {
-    const [movies, setMovies] = useState([]);
+    const [topRatedMovies, setTopRatedMovies] = useState([]);
+    const [popularMovies, setpopularMovies] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        async function loadMovies() {
-            const response = await api.get("/movie/top_rated", {
+        async function loadMovies(movieType) {
+            const response = await api.get(`/movie/${movieType}`, {
                 params: {
                     api_key: "7a8e988df0c98a78f5908610d79cf071",
                     language: "pt-BR",
                     page: 1,   
                 }
             })
-            // console.log(response.data.results.slice(0,10))
-            setMovies(response.data.results.slice(0,10));
-            setLoading(false);
+            switch(movieType) {
+                case 'top_rated':
+                    setTopRatedMovies(response.data.results.slice(0,20));
+                    setLoading(false);
+                case 'popular':
+                    setpopularMovies(response.data.results);
+                    setLoading(false);
+            }
         }
-        loadMovies();
+        loadMovies("top_rated");
+        loadMovies("popular");
     }, [])
+
+    function moveToRight(item){
+        document.querySelector(`.${item} ul`).scrollBy({top: 0, left: 900, behavior: 'smooth' })
+    }
+    
+    function moveToLeft(item){
+        document.querySelector(`.${item} ul`).scrollBy({top: 0, left: -900, behavior: 'smooth' })
+    }
 
     if(loading){
         return(
@@ -34,16 +52,43 @@ function Home() {
     }
     return(
         <div className="container">
-            <div className="lista-filmes">
-                {movies.map((movie) => {
-                    return (
-                        <article key={movie.id}>
-                            <strong>{movie.title}</strong>
-                            <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title}></img>
-                            <Link to={`/filme/${movie.id}`}>Acessar</Link>
-                        </article>
-                    )
-                })}
+            <div className="lista-filmes top-rated">
+                <div className="carousel-button left" onClick={() => moveToLeft('top-rated')}>
+                    <FontAwesomeIcon icon={faChevronLeft}/>
+                </div>
+                <ul>
+                    {topRatedMovies.map((movie) => {
+                        return (
+                            <li>
+                                <article key={movie.id}>
+                                    {/* <strong>{movie.title}</strong> */}
+                                    <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={movie.title}></img>
+                                    <i class="fa-solid fa-chevron-right"></i>
+                                    {/* <Link to={`/filme/${movie.id}`}>Acessar</Link> */}
+                                </article>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <div className="carousel-button right" onClick={() => moveToRight('top-rated')}>
+                    <FontAwesomeIcon icon={faChevronRight}/>
+                </div>
+            </div>
+            <div className="lista-filmes popular">
+                <ul>
+                    {popularMovies.map((movie) => {
+                        return (
+                            <li>
+                                <article key={movie.id}>
+                                    <strong>{movie.title}</strong>
+                                    {/* <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title}></img> */}
+                                    <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title}></img>
+                                    <Link to={`/filme/${movie.id}`}>Acessar</Link>
+                                </article>
+                            </li>
+                        )
+                    })}
+                </ul>
             </div>
         </div>
     )
