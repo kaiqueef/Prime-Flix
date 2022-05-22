8b74bec573cc29820128a3dde7f90fb9f520becc
Kaique <kaique@neurony.com.br>
2022-05-22 15:06:31 -0300

    Acerta página não encontrada

--- src/pages/Erro/index.js
+++ src/pages/Erro/index.js
@@ -6,7 +6,7 @@ function Erro() {
         <div className="not-found">
             <h1>404</h1>
             <h2>Pagina não encontrada</h2>
-            <Link to="/">Veja todos os filmes!</Link>
+            <Link className="button" to="/">Veja todos os filmes!</Link>
         </div>
     )
 }
