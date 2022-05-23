3f72a7d5de41cf012ab314ce0b0b2e54aefe2cdb
Kaique <kaique@neurony.com.br>
2022-05-22 23:55:39 -0300

    Conserta hover em filmes em desktop

--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -9,10 +9,10 @@ function MovieList(props) {
     const mobileBannerWidth = 300;
     const carouselButtonWidth = 50;
     const [scrollWidth, setScrollWidth] = useState(900);
-
+    
     function calculateScrollSize() {
-        const { innerWidth: width } = window;
-        return Math.floor((width - carouselButtonWidth*2)/mobileBannerWidth)*mobileBannerWidth || mobileBannerWidth;
+        const { innerWidth: windowWidth } = window;
+        return Math.floor((windowWidth - carouselButtonWidth*2)/mobileBannerWidth)*mobileBannerWidth || mobileBannerWidth;
     }
 
     useEffect (() => {
@@ -28,6 +28,23 @@ function MovieList(props) {
         document.querySelector(`.${item} ul`).scrollBy({top: 0, left: -scrollWidth, behavior: 'smooth' })
     }
 
+    function checkPosition(event) {
+        const mousePosition = event.nativeEvent.clientX;
+        if (mousePosition < mobileBannerWidth+carouselButtonWidth){
+            event.currentTarget.parentNode.style.left = 0;
+            return
+        }
+        const elementRightPosition = event.currentTarget.parentElement.getBoundingClientRect().right;
+        const { innerWidth: windowWidth } = window;
+        const elementWidthOutsideScreen = windowWidth - elementRightPosition;
+        if ( elementWidthOutsideScreen < 20) {
+            event.currentTarget.parentNode.style.left = `calc(${elementWidthOutsideScreen}px - 76px)`
+        }
+    }
+    function restorePosition(event) {
+        event.currentTarget.parentNode.style.removeProperty('left')
+    }
+
     return(
         <div className={`lista-filmes ${props.class}`}>
             <h2>{props.title}</h2>
@@ -40,13 +57,15 @@ function MovieList(props) {
                         <li key={movie.id}>
                             <article >
                                 <div>
-                                    <Link to={`/filme/${movie.id}`}>
+                                    <Link onMouseEnter={(event) => {checkPosition(event)}} onMouseLeave={(event) => restorePosition(event)} to={`/filme/${movie.id}`}>
                                         <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={movie.title}></img>
                                     </Link>
-                                    <h3>{movie.title}</h3>
                                     <div>
-                                        <FontAwesomeIcon icon={faStar}/>
-                                        <h4>IMDb: {movie.vote_average} / 10</h4>
+                                        <h3>{movie.title}</h3>
+                                        <div>
+                                            <FontAwesomeIcon icon={faStar}/>
+                                            <h4>IMDb: {movie.vote_average} / 10</h4>
+                                        </div>
                                     </div>
                                 </div>
                             </article>
